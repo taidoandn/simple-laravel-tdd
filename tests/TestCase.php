7f0c7866b019cf923c078e7eabc8ae57a37444bb
Tai Doan <doanchitaidn@gmail.com>
2021-04-13 15:58:14 +0700

    Clean api request with apiAs

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function apiAs($method, $uri, array $data = [], array $headers = [], $user = null)
+    {
+        $headers = array_merge(
+            ['Authorization' => 'Bearer ' . JWTAuth::fromUser($user ?? factory(User::class)->create())],
+            $headers
+        );
+
+        return $this->json($method, $uri, $data, $headers);
+    }
 }
